a2db55239b64d4db5a023d6d237df8037d4bc6ca
Crowdbotics <team@crowdbotics.com>
2023-06-21 21:42:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-sun-41660",
+  url: "https://rapid-sun-41660.botics.co",
+  api: "https://rapid-sun-41660.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
